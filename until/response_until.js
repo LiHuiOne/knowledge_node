0d7216841903d31d3fd94b5c66cd153297497121
lihui_lh <OnfEnSQSwtepmbOgttHC>
2021-09-17 17:16:58 +0800

    feat(优化请求返回数据格式)代码统计

--- until/response_until.js
+++ until/response_until.js
@@ -1,10 +1,16 @@
 //返回的数据，可以调用方法返回统一格式的数据
 const responseUntil = function(result){
     console.log(result)
+    let responseData=null
     let resObj = {
         status:result?'ok':'fail'
     }
-    let responseData=Object.assign({},resObj,result)
+    if(Array.isArray(result)){
+        responseData=Object.assign({},resObj,{data:result})
+    }else{
+        responseData=Object.assign({},resObj,result)
+    }
+    
     console.log(responseData)
     return responseData
 }

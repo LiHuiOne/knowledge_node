369478960ac504dd3e9d78119a14c53ba6d54b62
lihui_lh <OnfEnSQSwtepmbOgttHC>
2021-09-23 13:54:52 +0800

    feat(添加echarts修改和删除接口)代码提交

--- route/echarts_router.js
+++ route/echarts_router.js
@@ -3,12 +3,14 @@ const echartsCollection = require('../model/echrts_model.js')
 const echartsRouter = express.Router();
 const responseUntil = require('../until/response_until.js')
 
+//新增
 echartsRouter.post('/save',(req,res,next)=>{
     echartsCollection.create(req.body).then(result=>{
         res.send(responseUntil(result))
     })
 })
 
+//列表
 echartsRouter.get('/index',(req,res,next)=>{
     const skipCount = (parseInt(req.query.currentPage-1))*parseInt(req.query.pageSize)
     echartsCollection.find().then(response=>{
@@ -23,5 +25,25 @@ echartsRouter.get('/index',(req,res,next)=>{
     })
 })
 
+//修改
 
+echartsRouter.post('/modify',(req,res,next)=>{
+    let obj={
+        name:req.body.name,
+        type:req.body.type,
+        description:req.body.description,
+        code:req.body.code,
+        imageSrc:req.body.imageSrc
+    }
+    echartsCollection.updateOne({_id:req.body._id},obj).then(result=>{
+        res.send(responseUntil(result))
+    })
+})
+
+//删除
+echartsRouter.post('/delete',(req,res,next)=>{
+    echartsCollection.deleteOne({_id:req.body.id}).then(result=>{
+        res.send(responseUntil(result))
+    })
+})
 module.exports=echartsRouter
